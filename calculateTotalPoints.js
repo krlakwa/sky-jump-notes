01d338b9dbd2bf92ac70d5ea5e82b754745f278c
Krzysztof Łakwa <fenrir14@gmail.com>
2018-07-27 09:41:48 +0200

    Fix error in floating point numbers

--- calculateTotalPoints.js
+++ calculateTotalPoints.js
@@ -5,8 +5,9 @@ const calculateTotalPoints = (distance, hillSize, kPoint, styleNotes, windFactor
   const distancePoints = calculateDistancePoints(distance, hillSize, kPoint);
   const stylePoints = calculateStylePoints(styleNotes);
 
-  return distancePoints + stylePoints + windFactor + gateFactor;
+  return (distancePoints * 10 + stylePoints * 10 + (windFactor * 10 + gateFactor * 10)) / 10;
 };
 
 
+console.log(calculateTotalPoints(227.5, 'flying', 200, [18, 18.5, 17.5, 18.5, 18.5], -8.4, 8.7));
 module.exports = calculateTotalPoints;
